c793db75994ced5ef7e08ed154a205929212482a
AlexandrASUTP <sasha.krivosheev.82@mail.ru>
2022-06-15 12:16:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telegram_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
